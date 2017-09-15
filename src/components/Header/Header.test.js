2cb882bbfaed51a285df5998140b05e5776f2e60
Hai Pham <haipham23@gmail.com>
2017-09-15 15:24:06 +0800

    add test for NotFound

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
+
+import Header from './Header';
+
+describe('Header', () => {
+  const wrapper = mount(
+    <Header />
+  );
+
+  it('should render', () => {
+    expect(wrapper.find('.tm-header').length).to.equal(1);
+    expect(wrapper.find('h2').length).to.equal(1);
+    expect(wrapper.find('h2').text()).to.equal('Text Master Supermarket');
+  });
+});
--- src/components/NotFound/NotFound.test.js
+++ src/components/NotFound/NotFound.test.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { expect } from 'chai';
 
-import Header from './Header';
+import NotFound from './NotFound';
 
-describe('Header', () => {
+describe('NotFound', () => {
   const wrapper = mount(
-    <Header />
+    <NotFound />
   );
 
   it('should render', () => {
+    expect(wrapper.find('.nf').length).to.equal(1);
     expect(wrapper.find('h2').length).to.equal(1);
-    expect(wrapper.find('h2').text()).to.equal('Text Master Supermarket');
+    expect(wrapper.find('h2').text()).to.equal('Page Not Found');
   });
 });
