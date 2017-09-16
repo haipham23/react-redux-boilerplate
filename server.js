94d69d53e902dc4711d26b7671dfc2c5960d851e
Hai Pham <haipham23@gmail.com>
2017-09-16 22:07:43 +0800

    fix express issue on heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const compression = require('compression');
 const helmet = require('helmet');
 const sslRedirect = require('heroku-ssl-redirect');
 const getenv = require('getenv');
+const path = require("path");
 
 const port = getenv('PORT', 3000);
 const app = express();
@@ -10,6 +11,6 @@ const app = express();
 app.use(helmet());
 app.use(compression());
 app.use(sslRedirect());
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.listen(port);
